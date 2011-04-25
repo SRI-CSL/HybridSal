af565c434918269e5143abbb2a5218b7d99eae70
tiwari <tiwari@598a3e1c-6b6c-e011-a152-003048836090>
2011-04-25 00:36:18 +0000

    git-svn-id: svn+ssh://rfs.csl.sri.com/svn/internal/ashish-tools@3 598a3e1c-6b6c-e011-a152-003048836090

--- src/HSalRelAbsCons.py
+++ src/HSalRelAbsCons.py
@@ -35,6 +35,12 @@ def multiplyAv(A,v):
         res.append(dotproduct(i,v))
     return res
 
+def scale(v, a):
+    "return a.v where a is a scalar, v a vector"
+    for i in range(len(v)):
+        v[i] = a * v[i]
+    return v
+
 def modulus(v):
     "return modulus of the vector v"
     sum = 0
@@ -43,7 +49,7 @@ def modulus(v):
     return(pow(sum,0.5))
 
 def normalize(v):
-    "return Av ./ |v|"
+    "return v ./ |v|"
     modv = modulus(v)
     ans = list(v)
     for i in range(len(v)):
@@ -57,10 +63,16 @@ def Avbyv(A,v):
 def equal(c,d):
     return(abs(c-d) < epsilon)
 
+def equalVector(c,d):
+    for i in range(len(c)):
+        if not(equal(c[i], d[i])):
+            return 0
+    return 1
+
 def noteq(c,d):
     return(not(equal(c, d)))
 
-def eigenvalue(A):
+def eigenvalueLargest(A):
     "return largest eigenvalue of A, iterated method"
     #newA = exponent(A,10)
     i = 0
@@ -78,7 +90,7 @@ def eigenvalue(A):
     print "lambold %f" % lambold
     print normalize(v)
     print normalize(multiplyAv(A,v))
-    return(lamb)
+    return([lamb,v])
 
 def zeros(A):
     x = list(A)
@@ -137,27 +149,92 @@ def solve(A,b):
         #print b 
     return(extractSoln(A,b))
 
+def minusUV(u,v):
+    for j in range(len(u)):
+        u[j] = u[j] - v[j]
+    return u
+
+def minusAB(A,B):
+    "Return A-B, destructive update of A"
+    n = len(A)
+    for i in range(n):
+        A[i] = minusUV(A[i], B[i])
+    return A
+
 def AminuslambI(A,lamb):
     "A-lamb*I"
     B = list(A)
     for i in range(len(A)):
         B[i][i] = A[i][i] - lamb
-    return(solve(B,zeros(A[i])))
+    return(B)
 
 def eigenvector(A,lamb):
     "return vector in kernel of A-lamb*I"
     B = AminuslambI(A,lamb)
-    b = list(A[0])
-    for i in range(len(b)):
-        b[i] = 0
-    return solve(B,b)
+    return solve(B,zeros(A[0]))
+
+def project(v, w):
+    "return (v.w) w"
+    p = dotproduct(v, w)
+    return scale(w, p)
+
+def projectOut(v, subspace):
+    "Return v - projection of v on subspace"
+    for i in range(len(subspace)):
+        w = project(v, list(subspace[i]))
+        v = minusUV(v, w)
+        del w
+        v = normalize(v) 
+    return v
+
+def inSubspace(v, subspace):
+    "Return true if v is in Subspace spanned by subspace
+     Assuming that v,subspace vectors are all normalized"
+    v = projectOut(v, subspace)
+    zero = zeros(v)
+    if equalVector(v, zero):
+        return None
+    else:
+        return v
+    return v
+
+def orbit(A, v):
+    "Return subspace(v,Av,A^2v,...)"
+    subspace = list()
+    v = normalize(v)
+    w = inSubspace(v, subspace)
+    while not(w == None):
+        subspace.append(w)
+        v = multiplyAv(A, w)
+        v = normalize(v)
+        w = inSubspace(v, subspace)
+    return subspace
+
+def eigenvalues(A):
+    "Return MODULUS of all eigenvalues of A"
+    n = len(A[0])
+    if n == 1:
+        ans = list()
+        return ans.append(A[0][0])
+    for i in range(n):
+        if isUnitColumn(A,i,n):
+            newA = removeRowColumn(A,i)
+            eigens = eigenvalues(newA)
+            eigens.append(A[i][i])
+            return eigens
+    [lamb, vec] = eigenvalueLargest(A)
+    subspace = orbit(A, vec)
+    eigenv = eigenvector(A, lamb)
+    if isZeroVector(eigenv):
+        eigenv = vec
 
 def test1():
     xx = [ [1,2,3], [0,2,6], [0,0,3] ]
     #print multiplyAv(xx,[0,0,1])
     print "Test of largest eigevalue computation of:"
     print xx
-    print eigenvalue(xx) 
+    [lamb, v] = eigenvalueLargest(xx) 
+    print lamb
     print "The largest eigenvalue should be 3"
 
 def test2():
