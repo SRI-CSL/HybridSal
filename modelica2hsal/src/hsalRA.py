56fae80f48fbdaad8e24d9ae35bdcf421e085ddc
tiwari <tiwari@598a3e1c-6b6c-e011-a152-003048836090>
2014-02-28 08:47:39 +0000

    git-svn-id: svn+ssh://rfs.csl.sri.com/svn/internal/ashish-tools@251 598a3e1c-6b6c-e011-a152-003048836090

--- modelica2hsal/src/hsalRA.py
+++ modelica2hsal/src/hsalRA.py
@@ -1,4 +1,3 @@
-# OBSOLETE FILE -- use modelica2sal instead....
 import sys
 import os.path
 import inspect
@@ -20,41 +19,21 @@ def printUsage():
     print '''
 modelica2sal -- a converter from Modelica to Sal
 
-Usage: python modelica2sal.py <modelica_file.xml> [<context_property.xml>]
+Usage: python modelica2sal.py <modelica_file.xml> [<context_property.xml>] [--addTime]
 
 Description: This will create a file called modelica_fileModel.sal
     '''
 
-# def generateHSalfilename(filename):
-    # basename,ext = os.path.splitext(filename)
-    # basename = basename.replace('.','_')
-    # basename += "Model"
-    # return basename + ".hsal"
-
-def hsal2hxml(filename):
-    basename,ext = os.path.splitext(filename)
-    if ext == '.hxml':
-        xmlfilename = filename
-    elif ext == '.hsal':
-        xmlfilename = basename + ".hxml"
-        hybridsal2xml = 'hybridsal2xml.jar'
-        retCode = subprocess.call(["java", "-jar", hybridsal2xml, "-o", xmlfilename, filename])
-        if retCode != 0 or not(os.path.isfile(xmlfilename)):
-            print "hybridsal2xml failed to create XML file. Quitting."
-            return 1
-    else:
-        print "Unknown file extension; Expecting .hsal or .hxml; Quitting"
-        return 1
-    return xmlfilename
-
 def main():
+    def getexe():
+        folder = os.path.split(inspect.getfile( inspect.currentframe() ))[0]
+        relabsfolder = os.path.join(folder, '..', '..', 'bin')
+        relabsfolder = os.path.realpath(os.path.abspath(relabsfolder))
+        return relabsfolder
     global dom
     (filename, pfilename) = modelica2hsal.argCheck(sys.argv, printUsage)
     try:
         outfile = modelica2hsal.modelica2hsal(filename, pfilename, options = sys.argv[1:])
-        # maybe copy file to a local place?
-        #retCode = subprocess.call(['modelica2hsal', filename, pfilename])
-        #outfile = generateHSalfilename(filename)
     except Exception, e:
         print e
         print 'Error: Unable to create HybridSal file from Modelica XML'
@@ -62,35 +41,29 @@ def main():
     # this will create outfile == 'filenameModel.hsal'
     # now I need to run hsal2hasal
     try:
-        xmlfilename = hsal2hxml(outfile)
+        xmlfilename = HSalRelAbsCons.hsal2hxml(outfile)
     except Exception, e:
         print e
         print 'Syntax error in generated HybridSal file. Quitting.'
         return -1
     try:
-        ans = HSalRelAbsCons.hxml2sal(xmlfilename, optarg=0, timearg=None)
+        ans, prop_exists = HSalRelAbsCons.hxml2sal(xmlfilename, optarg=0, timearg=None)
     except Exception, e:
         print e
         print 'Relational abstracter can not abstract this model. Quitting.'
         return -1
     iswin = sys.platform.startswith('win')
-    if pfilename != None:
+    # if pfilename != None:
+    if prop_exists:
         salinfbmc = 'sal-inf-bmc'
         if iswin:
-            salinfbmc += '.bat'
+            salinfbmc = os.path.join(getexe(), 'sal-inf-bmc.bat')
         # change directory to where the sal file was created...
-        # os.chdir( os.path.dirname(ans) )
-        ans = os.path.realpath(os.path.abspath(ans))
-        ans = repr(ans)
-        ans = ans.replace('\\','/')
-        ans = ans.replace('c:/','/cygdrive/c/')
-        ans = ans[1:-1]
-        retCode = subprocess.call([salinfbmc, "-d", "4", ans, "p1"])
+        os.chdir( os.path.dirname(ans) )
+        retCode = subprocess.call([salinfbmc, "-d", "4", os.path.basename(ans), "p1"])
         if retCode != 0:
             print "sal-inf-bmc failed."
             return 1
-        else:
-            print "sal-inf-bmc terminated successfully."
         #print 'You can now run sal-inf-bmc on the generated SAL file'
         #print 'Use the command: sal-inf-bmc -d 4 <GeneratedSALFile> p1'
         #print 'NOTE: The requirement is called p1 in the generated SAL file'
