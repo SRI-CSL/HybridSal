e6ff21bd0b92d4e4779c975e1328c822005c38e6
tiwari <tiwari@598a3e1c-6b6c-e011-a152-003048836090>
2014-05-09 00:53:43 +0000

    git-svn-id: svn+ssh://rfs.csl.sri.com/svn/internal/ashish-tools@269 598a3e1c-6b6c-e011-a152-003048836090

--- setup.py
+++ setup.py
@@ -7,10 +7,11 @@ import os.path
 
 sys.path.insert(0, os.path.join('modelica2hsal','src'))
 sys.path.insert(0, os.path.join('src'))
+sys.path.insert(0, os.path.join('cc2hsal','src'))
 
 #setup(console=['modelica2hsal/src/modelica2sal.py'], packages = ['src','modelica2hsal.src'])
 #setup(console=['modelica2hsal/src/modelica2sal.py'], package_dir = {'':'src'})
 #setup(console=['modelica2hsal/src/modelica2sal.py'], packages = ['../modelica2hsal/src'], packages_dir = {'':'src'})
 #setup(console=['modelica2hsal/src/modelica2sal.py'], py_modules = ['src/HSalRelAbsCons','modelica2hsal/src/modelica2hsal'])
 #setup(console=['modelica2hsal/src/modelica2sal.py'])
-setup(console=['modelica2hsal/src/hsalRA.py'])
+setup(console=['modelica2hsal/src/hsalRA.py', 'cc2hsal/src/cc_hra_verifier.py'])
