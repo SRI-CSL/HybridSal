fa010d16505e63091d078116cdfea19712d2108c
tiwari <tiwari@598a3e1c-6b6c-e011-a152-003048836090>
2014-08-19 18:39:36 +0000

    git-svn-id: svn+ssh://rfs.csl.sri.com/svn/internal/ashish-tools@283 598a3e1c-6b6c-e011-a152-003048836090

--- modelica2hsal/src/modelica_slicer.py
+++ modelica2hsal/src/modelica_slicer.py
@@ -7,16 +7,110 @@ import xml.dom.minidom
 # handling of kvars at line 356 - please improve
 # for all kvars; just print variablevalue pairs.
 # for all missing ones; raise an alarm
-# 
+# 5 Aug, 2014: Using only orderedVariables for creating slices...
 
+# 17 Aug, 2014: initialEquations absent in slice
+# 17 Aug, 2014: aliasVariables  absent in slice
+
+# Notes for SLICER:
+# 2. Maintain equivalence class of variables
+#  if x+y=10 is an equation, then x,y are in the same class
+#  if dx/dt = x+y+z, then x points_to y,z
+# compute influence variables modulo equivalence...
+
+# ----------------------------------------------------------------------
+# Union Find data structure
+# ----------------------------------------------------------------------
+class UnionFind:
+  def __init__(self):
+    self.parent = {}
+  def find(self, v):
+    ans = v
+    while self.parent.has_key(ans):
+      ans = self.parent[ans]
+    u = v
+    while u != ans and self.parent[u] != ans:
+      tmp = self.parent[u]
+      self.parent[u] = ans
+      u = tmp
+    return ans
+  def union(self, varlist): 
+    if varlist == []:
+      return self
+    vroot = self.find( varlist[0] )
+    for v in varlist[1:]:
+      vroot1 = self.find(v)
+      if vroot1 != vroot:
+        self.parent[vroot1] = vroot
+    return self
+  def get_eq_class(self, v):
+    '''return list of all v's that are eq to given v'''
+    v3 = self.find(v)
+    ans = [ v3 ]
+    print 'eq class representative: {0}'.format(v3)
+    for (v1,v2) in self.parent.items():
+      if v2==v3:
+        ans.append(v1)
+    return ans
+  def __str__(self):
+    classes = {}
+    ans = '{\n'
+    for k in self.parent.keys():
+      v = self.find(k)
+      if classes.has_key(v):
+        classes[v] += 1
+      else:
+        classes[v] = 1
+      ans += ' {0}:{1},\n '.format( k,v)
+    ans += '}'
+    ans += str(classes)
+    return ans
+# ----------------------------------------------------------------------
+
+# ----------------------------------------------------------------------
+# Binary Relation -- dependency relation for slicing
+# ----------------------------------------------------------------------
+class LabeledBinaryRelation:
+  def __init__(self):
+    self.r = {}
+  def update(self, src, dst, label):
+    if self.r.has_key(src):
+      if self.r[src].has_key(dst):
+        return self
+      else:
+        self.r[src][dst] = label
+    else:
+      self.r[src] = {dst:label}
+    return self
+  def normalize(self, uf):
+    def normalize1(vlabel, uf):
+      return { uf.find(k):v for k,v in vlabel.items() }
+    for k,v in self.r.items():
+      knf = uf.find(k)
+      if self.r.has_key(knf):
+        self.r[knf].update( normalize1(v,uf) )
+      else:
+        self.r[knf] = normalize1(v,uf)
+  def image(self, v):
+    if self.r.has_key(v):
+      return self.r[v]
+    return {}
+  def __str__(self):
+    return str(self.r)
+# ----------------------------------------------------------------------
+
+# ----------------------------------------------------------------------
+# Usage 
+# ----------------------------------------------------------------------
 def printUsage():
     print '''
-modelica2daexml -- a converter from Modelica to internal daexml file
+modelica_slicer -- a slicer from Modelica DAEs; creates sliced DAE XML dump
 
-Usage: python src/modelica2daexml.py <modelica_file.xml> [--addTime|--removeTime]
+Usage: python src/modelica_slicer.py <modelica_file.xml> --slicewrt "v1,v2,v3"
 
-Description: This will create a file called modelica_file.daexml
+Description: This will create a file called modelica_file_slice.xml
     '''
+# -------------------------------------------------------------------
 
 # -------------------------------------------------------------------
 # Helper functions
@@ -27,16 +121,6 @@ def valueOf(node):
     for i in node.childNodes:
         if i.nodeType == i.TEXT_NODE:
             return(i.data)
-
-def getNameTag(node, tag):
-    nodes = node.getElementsByTagName(tag)
-    if (len(nodes) < 1):
-        print >> sys.stderr, node.toxml()
-    childnode = nodes[0]
-    return(valueOf(childnode))
-
-def getName(node):
-    return getNameTag(node, "IDENTIFIER")
 
 def getChildByTagName(node,tag):
     for i in node.childNodes:
@@ -88,24 +172,6 @@ def helper_collect_child(node, base=0):
 # -------------------------------------------------------------------
 
 # -------------------------------------------------------------------
-# mathml2localxml
-def mathml2localxml(dom1):
-    "dom1 = daexml DOM that uses MathML; rewrite mathml to my local XML on which simplifier works"
-    mmls = dom1.getElementsByTagName('MathML')
-    for i in mmls:
-        j = mathml2myxml(i)
-        if j == None:
-            # use string attribute
-            print 'No MathML expression found; parsing string expression'
-            estr = i.getAttribute('string')
-            j = ddae.parse_expr( estr )
-        replace(i, j)
-    return dom1
-
-def replace(node, newnode):
-    parentnode = node.parentNode
-    parentnode.replaceChild(newChild=newnode,oldChild=node)
-
 def grand_children_count(node):
     child = getArg(node,1)
     childs = getArgs(child)
@@ -408,7 +474,227 @@ def getVarValFromAlgo( simpleEquations, leftOutVars1 ):
     leftOutVars = [ i for i in leftOutVars1 if i not in done_vars ]
     return (varvals, leftOutVars)
 
-def modelicadom2daexml(modelicadom):
+# Note: there is also "aliasVariables"
+def varsxml2dict(ovars):
+  '''return a dict from var-name to var-id'''
+  ans = {}
+  for i in ovars:
+    ans[i.getAttribute('name')] = i.getAttribute('id')
+  return ans
+
+def kvars2dict(kvars, uf, reliesOn, ovarl, prefix='k'):
+  ans = {}
+  for var in kvars:
+    eid = var.getAttribute('id')
+    vname = var.getAttribute('name')
+    bindvalexpr = getChildByTagName(var, 'bindValueExpression')
+    if bindvalexpr == None:
+      continue
+    bindexpr = getChildByTagName(bindvalexpr, 'bindExpression')
+    if bindexpr == None:
+      continue
+    elist = [ vname ]
+    mml = getChildByTagName(bindexpr, 'MathML')
+    assert mml != None, 'No MathML'
+    lhs = helper_create_tag_val('ci', vname)
+    analyze_mml( lhs, mml, uf, reliesOn, ovarl, name=prefix+eid )
+  return (uf, reliesOn)
+
+def mml_eqn_get_lhs_rhs( mml ):
+  '''MathML equation: <MathML><math>
+     <apply><equiv><lhs><rhs></apply><math></MathML>'''
+  math_e = getChildByTagName(mml, 'math')
+  assert math_e != None, 'No math in MathML'
+  apply_e = getChildByTagName(math_e, 'apply')
+  assert apply_e != None, 'No apply in MathML.math'
+  equiv = getArg( apply_e, 1)
+  #print apply_e.toxml(), equiv.toxml()
+  assert equiv.tagName == 'equivalent', 'Err: Expected equivalent tag {0}\n{1}'.format(equiv.toxml(), apply_e.toxml())
+  lhs = getArg( apply_e, 2)
+  rhs = getArg( apply_e, 3)
+  return (lhs,rhs)
+
+def eqnsxml2dict(eqns, uf, reliesOn, ovarl, prefix=''):
+  '''get its MathML child, find <ci> in it'''
+  for e in eqns:
+    eid = e.getAttribute('id')
+    mml = getChildByTagName(e, 'MathML')
+    assert mml != None, 'No MathML'
+    lhs, rhs = mml_eqn_get_lhs_rhs(mml)
+    analyze_mml( lhs, rhs, uf, reliesOn, ovarl, name=prefix+eid )
+    '''cis = mml.getElementsByTagName('ci')
+    for i in cis:
+      varname = valueOf(i).strip()
+      if varname not in elist:
+        elist.append(varname)
+    ans[ prefix+eid ] = elist
+    '''
+  return (uf, reliesOn)
+
+def analyze_mml( lhs, rhs, uf, reliesOn, ovarl, name='e'):
+  '''update uf and reliesOn based on lhs=eqn, use name as label'''
+  nxt, curr, pre = [], [], []
+  (nxt, curr, pre) = classify_vars_as_curr_pre( lhs, nxt, curr, pre, ovarl)
+  (nxt, curr, pre) = classify_vars_as_curr_pre( rhs, nxt, curr, pre, ovarl)
+  '''
+  if nxt != []:
+    print 'name={0}, nxt={1}, curr={2}, pre={3}'.format(name, nxt, curr, pre)
+  '''
+  if nxt == [] and pre == []:
+    uf.union( curr )
+  elif pre == []:
+    for i in nxt:
+      for j in curr:
+        reliesOn.update(i, j, name)
+  elif nxt == []:
+    for i in curr:
+      for j in pre:
+        reliesOn.update(i, j, name)
+  else:
+    assert False, 'Err: One equation has all next, curr and pre vars!'
+  return (uf, reliesOn)
+
+def classify_vars_as_curr_pre( mml, nxt, curr, pre, ovarl ):
+  '''recurse and clasify each var in mml expr as nxt, curr or pre'''
+  if mml.tagName == 'ci':
+    allvars = [ mml ]
+  else:
+    allvars = mml.getElementsByTagName('ci')
+  for i in allvars:
+    varname = valueOf(i).strip()
+    if varname not in ovarl:
+      continue
+    parent = i.parentNode
+    if parent != None and parent.tagName == 'apply':
+      op = getArg(parent, 1)
+      if op.tagName == 'diff':
+        if varname not in nxt:
+          nxt.append(varname)
+      elif op.tagName == 'pre':
+        if varname not in pre:
+          pre.append(varname)
+      else:
+        if varname not in curr:
+          curr.append(varname)
+    else:
+      if varname not in curr:
+        curr.append(varname)
+  '''print 'Expr and its classification of variables'
+  print mml.toxml()
+  print 'nxt ', nxt
+  print 'curr ', curr
+  print 'pre ', pre '''
+  return (nxt, curr, pre)
+
+def getEqnsContainingV(v, edict):
+  ans = []
+  for (e,vlist) in edict.items():
+    for va in vlist:
+      if va.endswith(v):
+        ans.append(e)
+        break
+  return ans
+
+def getAllVarsInE(e, edict):
+  return edict[e]
+
+def varbackwards( varlist, uf, reliesOn ):
+  '''monotonically increase varlist, list =list of names '''
+  todo = varlist
+  relevantv, relevante = [], []
+  while todo != []:
+    v = todo.pop()
+    if v not in relevantv:
+      relevantv.append(v)
+    else:
+      continue
+    vnf = uf.find( v )
+    vdict = reliesOn.image( vnf )
+    for v in vdict.keys():
+      if v not in todo and v not in relevantv:
+          todo.append( v )
+          relevante.append( vdict[v] )
+    print '#v, #e', len(relevantv), len(relevante)
+    print '#todo', len(todo)
+  return (relevantv, relevante)
+
+def find_matching_vars( varlist, ovarl ):
+  '''replace v in varlist by v' s.t. v' in ovarl and v'.endswith(v)'''
+  ans = []
+  for i in varlist:
+    istrip = i.strip()
+    print 'searching for {0}...'.format(istrip)
+    for j in ovarl:
+      if j.endswith(istrip):
+        print '{0} matched to {1}'.format(i, j)
+        ans.append(j)
+        break
+  return ans
+
+def extend_ovarl( ovarl, aliases ):
+  '''aliases...'''
+  ans = []
+  for i in aliases:
+    bind = i.getElementsByTagName('bindExpression')[0]
+    ci = bind.getElementsByTagName('ci')[0]
+    varname = valueOf(ci).strip()
+    if varname in ovarl:
+      ans.append(i.getAttribute('name'))
+  ovarl.extend( ans )
+  return ovarl
+
+# ----------------------------------------------------------------------
+# Slice of equations, given relevant variables and state variables list
+# ----------------------------------------------------------------------
+def isRelevantE( e, relevantv, ovarl):
+  '''return True if
+  for every var v in e, if v is in ovarl, then v is in relevantv
+  '''
+  ciL = e.getElementsByTagName('ci')
+  varsL = [ valueOf(i).strip() for i in ciL ]
+  restv = []
+  for v in varsL:
+    if v in ovarl:
+      if v not in relevantv:
+        return (False, restv)
+    else:
+      restv.append(v)
+  return (True, restv)
+
+def getRelevantE(equationL, total, ovarl):
+  '''return subset of equationL containing e s.t.
+  for every var v in e, if v is in ovarl, then v is in total
+  '''
+  ans, other_vars = [], []
+  for e in equationL:
+    (in_slice, restv) = isRelevantE( e, total, ovarl)
+    if in_slice:
+      ans.append(e)
+      other_vars.extend( restv )
+  return (ans, other_vars)
+# ----------------------------------------------------------------------
+
+# ----------------------------------------------------------------------
+def map_name_to_xml( var_name_l, ovars):
+  '''var_name_l = list of strings, ovars =list of XMLs'''
+  def find_var_in_list(var_name, ovars):
+    for i in ovars:
+      vname = i.getAttribute('name')
+      if vname == var_name:
+        return i
+    return None
+  sliced_v, rest = [], []
+  for i in var_name_l:
+    var_xml = find_var_in_list(i, ovars)
+    if var_xml != None:
+      sliced_v.append(var_xml)
+    else:
+      rest.append(i)
+  return (sliced_v, rest)
+# ----------------------------------------------------------------------
+
+# ----------------------------------------------------------------------
+def modelicadom2daexml(modelicadom, varlist):
     "dom = modelicaXML dom; output daexml DOM... WITH MathMLs now"
     global dom
     impl = xml.dom.minidom.getDOMImplementation()
@@ -416,18 +702,73 @@ def modelicadom2daexml(modelicadom):
     ctxt = modelicadom.getElementsByTagName('dae')[0]
     variables = getChildByTagName(ctxt, 'variables')
     equations = getChildByTagName(ctxt, 'equations')
-    # zeroCrossing = getChildByTagName(ctxt, 'zeroCrossingList')
-    # arrayOfEqns = getChildByTagName(ctxt, 'arrayOfEquations')
-    # algorithms = getChildByTagName(ctxt, 'algorithms')
-    # functions = getChildByTagName(ctxt, 'functions')
+    zeroCrossing = getChildByTagName(ctxt, 'zeroCrossingList')
+    arrayOfEqns = getChildByTagName(ctxt, 'arrayOfEquations')
+    algorithms = getChildByTagName(ctxt, 'algorithms')
+    functions = getChildByTagName(ctxt, 'functions')
     assert variables != None, 'Variables not found in input XML file!'
     # Now start printing .dae file
     tmp = getChildByTagName(variables, 'orderedVariables')
     ovars = tmp.getElementsByTagName('variable') if tmp != None else []
-    tmp = getChildByTagName(variables, 'knownVariables')
-    kvars = tmp.getElementsByTagName('variable') if tmp != None else []
+    ovarl = [i.getAttribute('name') for i in ovars]
+
+    kvars = []
     tmp = getChildByTagName(variables, 'externalVariables')
     kvars.extend( tmp.getElementsByTagName('variable') if tmp != None else [] )
+    tmp = getChildByTagName(variables, 'aliasVariables')
+    aliases = tmp.getElementsByTagName('variable') if tmp != None else []
+    kvars.extend( aliases )
+
+    ## update varlist given ovarl
+    ovarl = extend_ovarl( ovarl, aliases )
+    # ovarl.extend( [i.getAttribute('name') for i in aliases] )
+    varlist = find_matching_vars( varlist, ovarl )
+    # Now, do a LFP fixpoint on
+    # var,eqn -> newvar,neweqn adding keeping id's
+    equationL = equations.getElementsByTagName('equation')
+    union_find,  relies_on = UnionFind(), LabeledBinaryRelation()
+    (uf, reliesOn) = eqnsxml2dict(equationL, union_find, relies_on, ovarl)
+    # print 'reliesOn after equations before normalizing...'
+    # print reliesOn
+    (uf, reliesOn) = kvars2dict( kvars, uf, reliesOn, ovarl )
+    # print 'reliesOn before normalizing...'
+    # print reliesOn
+    reliesOn.normalize( uf )
+    # print 'Done creating UF and the reliesOn DS'
+    # print uf
+    # print reliesOn
+    (relevantv, relevante) = varbackwards( varlist, union_find, relies_on )
+    print 'No. of relevant nodes = {0}:'.format(len(relevantv))
+    print relevantv 
+    print 'No. of relevant equations = {0}:'.format(len(relevante))
+    print relevante
+    sliced_v, roots = [], []
+    for i in relevantv:
+      print 'Equivalence class of {0}'.format(i)
+      rooti = union_find.find( i )
+      if rooti in roots:
+        continue
+      roots.append( rooti )
+      eq_class = union_find.get_eq_class( i )
+      sliced_v.extend(eq_class)
+      print eq_class
+    print 'Num of different eq classes = {0}'.format(len(roots))
+    print 'Sum of all eq classes = {0}'.format(len(sliced_v))
+    # get all equations containing only vars in sliced_v
+    (slice_e, other_v) = getRelevantE(equationL, sliced_v, ovarl)
+    print 'Number of relevant equations = {0}'.format(len(slice_e))
+    # sliced_v -> name -> XML map using ovars
+    (sliced_v, rest) = map_name_to_xml( sliced_v, ovars)
+    tmp = getChildByTagName(variables, 'knownVariables')
+    kvars = tmp.getElementsByTagName('variable') if tmp != None else []
+    (other_v, rest2) = map_name_to_xml(other_v, kvars)
+    print 'Rest has {0} elements'.format(len(rest))
+    print 'Rest2 has {0} elements'.format(len(rest2))
+    return ( slice_e, sliced_v, other_v )
+    # sys.exit(1)
+    # return (relevantv, relevante)
+
+'''
     # print >> fp, '#####{0}'.format('continuousState')
     statevars = []
     for i in ovars:
@@ -517,6 +858,7 @@ def modelicadom2daexml(modelicadom):
     ans = helper_create_app('source_text', allnodes)
     dom.documentElement.appendChild(ans)
     return dom
+'''
 
 def argCheck(args, printUsage):
     "args = sys.argv list"
@@ -586,6 +928,38 @@ def removeTime(dom1):
             Eqn.removeChild(e)
     return dom1
 
+def output_sliced_dom(slice_filename, sliced_e, sliced_v, other_v, dom):
+  '''output XML in the given filename'''
+  with open(slice_filename, 'w') as fp:
+    print >> fp, '''
+<?xml version="1.0" encoding="UTF-8"?>
+<dae xmlns:p1="http://www.w3.org/1998/Math/MathML"
+    xmlns:xlink="http://www.w3.org/1999/xlink" 
+    xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" 
+    xsi:noNamespaceSchemaLocation="http://home.dei.polimi.it/donida/Projects/AutoEdit/Images/DAE.xsd">
+    '''
+    total_vars = len(sliced_v) + len(other_v)
+    print >> fp, '<variables dimension="{0}">'.format(total_vars)
+    print >> fp, '<orderedVariables dimension="{0}">'.format(len(sliced_v))
+    print >> fp, '<variablesList>'
+    for i in sliced_v:
+      print >> fp, i.toxml()
+    print >> fp, '</variablesList>'
+    print >> fp, '</orderedVariables>'
+    # Next print out knownVariables....
+    print >> fp, '<knownVariables dimension="{0}">'.format(len(other_v))
+    for i in other_v:
+      print >> fp, i.toxml()
+    # print other_v here????
+    print >> fp, '</knownVariables>'
+    print >> fp, '</variables>'
+    print >> fp, '</equations dimension="{0}">'.format(len(sliced_e))
+    for i in sliced_e:
+      print >> fp, i.toxml()
+    print >> fp, '</equations>'
+    print >> fp, '</dae>'
+  print 'Created file {0}'.format( slice_filename )
+
 def modelica2daexml(filename, options = []):
     def existsAndNew(filename1, filename2):
         if os.path.isfile(filename1) and os.path.getctime(filename1) >= os.path.getctime(filename2):
@@ -606,24 +980,20 @@ def modelica2daexml(filename, options = []):
     if '--addTime' in options:
         modelicadom = addTime(modelicadom)
     print >> sys.stderr, 'Creating .daexml file......'
+    if '--slicewrt' in options:
+      index = options.index('--slicewrt')
+      varlist = options[index+1].split(',')
+      (sliced_e, sliced_v, other_v) = modelicadom2daexml(modelicadom, varlist)
+      slice_filename = basename + '_slice.xml'
+      if not existsAndNew(slice_filename, filename):
+        # Bug: slice created using different variables have same filename
+        output_sliced_dom(slice_filename, sliced_e, sliced_v, other_v, modelicadom)
     # now parse the dae into daexml
-    daexmlfilename = basename + '.daexml'
-    if not existsAndNew(daexmlfilename, filename):
-        dom1 = modelicadom2daexml(modelicadom)
-        dom1 = mathml2localxml(dom1)
-        if '--removeTime' in options:
-            dom1 = removeTime(dom1)
-        with open(daexmlfilename, 'w') as fp:
-            print >> fp, dom1.toprettyxml()
-        print >> sys.stderr, 'Created file {0}'.format(daexmlfilename)
-    else: 
-        print >> sys.stderr, 'Using existing {0} file'.format(daexmlfilename)
-        try:
-            dom1 = xml.dom.minidom.parse(daexmlfilename)
-        except:
-            print 'Model not supported: Unable to handle some expressions currently'
-            sys.exit(-1)
-    return (modelicadom, dom1, daexmlfilename)
+    else:
+      print 'Specify which variables to slice for'
+      printUsage()
+      sys.exit(1)
+    return (slice_filename, modelicadom)
 
 if __name__ == "__main__":
     main()
